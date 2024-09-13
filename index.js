175d7bf0f5e816639062556a936a93c7b539db99
Dantas Ferreira Crispim <dantas10.crispim@gmail.com>
2024-09-13 19:57:15 -0300

    Menu da aplicação.

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 
-const start = () => {
+
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
     
 
     while(true) {
-        
-        let opcao = "sair"
+
+        // espere que o usuário vai selecionar alguma coisa...
+        const opcao = await select({      
+            message: "Menu  >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Metas",
+                    value: "listar"
+                },
+                {
+                    name:"Sair",
+                    value:"sair"
+                }
+            ]
+        })
+
+
         switch(opcao) {
-            case "Cadastrar":
-                console.log("vamos Cadastrar");
+            case "cadastrar":
+                console.log("Vamos Cadastrar");
                 break;
             case "listar":
-                console.log("vamos listar");
+                console.log("Vamos listar");
                 break;
             case "sair":
+                console.log("Até a próxima!")
                 return
             
         }
